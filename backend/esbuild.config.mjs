800eda8c25b1b9fd0a11b5d425d9fd514d333be7
Dmitrijs Pavlovs <dmitrijs.pavlovs123@gmail.com>
2024-03-18 15:35:30 +0200

    Fix missing config

--- backend/esbuild.config.mjs
+++ backend/esbuild.config.mjs
@@ -1,6 +1,7 @@
 import esbuild from 'esbuild'
 import esbuildPluginTsc from 'esbuild-plugin-tsc'
 import { dtsPlugin } from 'esbuild-plugin-d.ts'
+import copy from 'esbuild-plugin-copy'
 
 esbuild
   .build({
@@ -13,7 +14,14 @@ esbuild
       esbuildPluginTsc({
         force: true
       }),
-      dtsPlugin({})
+      dtsPlugin({}),
+      copy({
+        // Specify assets to be copied
+        assets: {
+          from: ['config/*'], // Matches all files in the 'config' directory
+          to: ['build/config'] // Copies them to 'build/config'
+        }
+      })
     ]
   })
   .catch(() => process.exit(1))
